b09b70c735c49b0d6a37178323e6b8f5fba99843
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-01-20 20:25:01 +0530

    Update votingadmin.js

--- models/votingadmin.js
+++ models/votingadmin.js
@@ -10,9 +10,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      VotingAdmin.hasMany(models.ElectionDetail,{
-        foreignKey:"userId"
-      })
     }
   }
   VotingAdmin.init({
